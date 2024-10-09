87591ac636658f45275473787763d8c705c9564c
Cnnn666 <83084283+cnnn666v2@users.noreply.github.com>
2024-10-09 17:31:57 +0200

    On branch main  Changes to be committed: 	modified:   index.php

--- index.php
+++ index.php
@@ -1,14 +1,5 @@
 <?php
-$servername = "localhost";
-$username = "root";
-$password = "";
-$dbname = "twoja_stara";
-
-$conn = mysqli_connect($servername, $username, $password, $dbname);
-
-if (!$conn) {
-    die("Connection failed: " . mysqli_connect_error());
-}
+include('technicalphp/connect.php');
 
 $aTitle = [];
 $aDesc = [];
