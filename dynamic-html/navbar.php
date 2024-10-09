6397d53201f31d40673daed2c1f2455a64395649
Cnnn666 <83084283+cnnn666v2@users.noreply.github.com>
2024-10-09 15:48:01 +0200

    On branch master  Changes to be committed: 	new file:   dynamic-html/navbar.php  Changes not staged for commit: 	modified:   css/output.css 	modified:   index.php  Untracked files: 	node_modules/ 	package-lock.json 	package.json

---
+++ dynamic-html/navbar.php
@@ -0,0 +1,12 @@
+<nav id="mainbar" class="w-full py-4 md:py-1 bg-slate-800 text-white list-none flex flex-row sticky top-0 z-10">
+    <h1 class="text-center self-center text-2xl normal-case ml-5"><span class="text-red-600">Red</span>Tech</h1>
+    <div class="m-2 md:flex flex-row gap-4 text-lg uppercase content-center hidden w-full">
+        <a class="p-3 text-gray-300 hover:bg-slate-700 hover:text-white rounded-lg font-semibold text-lg transition-all ease-in-out" href="#">Homepage</a>
+        <a class="p-3 text-gray-300 hover:bg-slate-700 hover:text-white rounded-lg font-semibold text-lg transition-all ease-in-out" href="#">News</a>
+        <a class="p-3 text-gray-300 hover:bg-slate-700 hover:text-white rounded-lg font-semibold text-lg transition-all ease-in-out" href="#">Blog</a>
+        <a class="p-3 text-gray-300 hover:bg-slate-700 hover:text-white rounded-lg font-semibold text-lg transition-all ease-in-out" href="#">Games</a>
+        <a class="p-3 ml-auto hover:bg-white hover:text-black border-2 border-white font-semibold text-base transition-all ease-in-out" href="#">Login</a>
+        <a class="p-3 hover:bg-white hover:text-black border-2 border-white font-semibold text-base transition-all ease-in-out" href="#">Register</a>
+    </div>
+    <button class="md:hidden flex flex-col gap-2 ml-auto mr-5 w-9 self-center" onclick="showMobile()"><span class="border-2 border-white"></span><span class="border-2 border-white"></span><span class="border-2 border-white"></span></button>
+</nav>
