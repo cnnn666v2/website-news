360ce5ddfce9936adfb8f412d154db52c1ae5a19
Cnnn666 <83084283+cnnn666v2@users.noreply.github.com>
2024-10-11 16:27:51 +0200

    On branch master  Your branch is up to date with 'website/master'.  Changes to be committed: 	modified:   css/output.css 	modified:   dynamic-html/navbar.php 	new file:   login.php

--- dynamic-html/navbar.php
+++ dynamic-html/navbar.php
@@ -5,8 +5,8 @@
         <a class="p-3 text-gray-300 hover:bg-slate-700 hover:text-white rounded-lg font-semibold text-lg transition-all ease-in-out" href="#">News</a>
         <a class="p-3 text-gray-300 hover:bg-slate-700 hover:text-white rounded-lg font-semibold text-lg transition-all ease-in-out" href="#">Blog</a>
         <a class="p-3 text-gray-300 hover:bg-slate-700 hover:text-white rounded-lg font-semibold text-lg transition-all ease-in-out" href="#">Games</a>
-        <a class="p-3 ml-auto hover:bg-white hover:text-black border-2 border-white font-semibold text-base transition-all ease-in-out" href="#">Login</a>
-        <a class="p-3 hover:bg-white hover:text-black border-2 border-white font-semibold text-base transition-all ease-in-out" href="#">Register</a>
+        <a class="p-3 ml-auto hover:bg-white hover:text-black border-2 border-white font-semibold text-base transition-all ease-in-out" href="login.php">Login</a>
+        <a class="p-3 hover:bg-white hover:text-black border-2 border-white font-semibold text-base transition-all ease-in-out" href="register.php">Register</a>
     </div>
     <button class="md:hidden flex flex-col gap-2 ml-auto mr-5 w-9 self-center" onclick="showMobile()"><span class="border-2 border-white"></span><span class="border-2 border-white"></span><span class="border-2 border-white"></span></button>
 </nav>
---
+++ login.php
@@ -0,0 +1,72 @@
+<?php
+include('technicalphp/connect.php');
+
+$aTitle = [];
+$aDesc = [];
+
+$sql = "SELECT title, description FROM articles ORDER BY id DESC LIMIT 10";
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+    while ($row = $result->fetch_assoc()) {
+        $aTitle[] = $row['title'];
+        $aDesc[] = (strlen($row['description']) > 250) ? substr($row['description'],0,250).'...' : $row['description'];
+    }
+}
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+        <title>Homepage</title>
+
+        <link href="css/output.css" rel="stylesheet">
+        <link href="css/fonts.css" rel="stylesheet">
+    </head>
+
+    <body class="bg-slate-900">
+        <?php include('dynamic-html/mobilenavbar.php'); include('dynamic-html/navbar.php'); ?>
+
+        <main class="flex flex-col h-full flex-wrap md:flex-nowrap w-full">
+            <div class="w-full flex justify-center my-3">
+                <h1 class="text-white text-center w-1/2 text-3xl uppercase">I have 2 sides:</h1>
+            </div>
+            <div class="flex flex-row w-full justify-center p-10 pt-0">
+                <form method="POST" class="flex flex-col text-white bg-slate-800 m-5 p-5 basis-1/4 mr-0 rounded-l-xl mt-0">
+                    <h1 class="text-3xl text-center">Welcome!</h1>
+
+                    <h2 class="text-2xl mt-7">Username:</h2>
+                    <input placeholder="Enter username..." type="text" class="text-lg my-4 p-2 rounded-xl bg-slate-600 placeholder-gray-400 focus:border-none" required>
+
+                    <h2 class="text-2xl">Password:</h2>
+                    <input placeholder="Enter password..." type="password" class="text-lg my-4 p-2 rounded-xl bg-slate-600 placeholder-gray-400 focus:border-none" required>
+                    <p class="text-gray-400">Forgot password? <a href="#" class="text-cyan-500 underline hover:text-cyan-300">Click here</a></p>
+
+                    <button type="submit" class="text-xl px-2 py-3 text-center mt-5 rounded-xl uppercase border-2 border-white hover:bg-white hover:text-black transition-all ease-in-out">Login</button>
+                </form>
+                <div class="flex flex-col text-white bg-slate-700 m-5 p-5 ml-0 basis-1/4 rounded-r-xl mt-0">
+                    <div class="flex flex-row">
+                        <p><span class="text-3xl normal-case"><span class="text-red-600">Red</span>Tech</span> is a website that, Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean finibus quam et nisl porta, ac aliquet est dictum. Phasellus ut mollis erat. Sed eu turpis sollicitudin, vulputate nisl vitae, lobortis mi.</p>
+                    </div>
+
+                    <div class="flex flex-col mt-auto gap-2">
+                        <p class="text-2xl uppercase">Wanna join us?</p>
+                        <a class="px-2 py-3 text-center uppercase hover:bg-white hover:text-black border-2 rounded-xl border-white text-xl transition-all ease-in-out" href="register.php">Register</a>
+                    </div>
+                </div>
+            </div>
+        </main>
+        
+        <?php include('dynamic-html/footer.php'); ?>
+
+        <script type="text/javascript" src="javascript/hamburger.js"></script>
+        <noscript>Please enable javascript for the website to function.</noscript>
+    </body>
+</html>
+
+<?php
+$conn->close();
+?>
